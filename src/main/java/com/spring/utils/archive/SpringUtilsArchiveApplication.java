e5373660a251beb4dcceb0b70a3a46d1b5ca5f4b
Michelanjeho <michelanjeho@gmail.com>
2022-05-18 16:25:27 +0900

    Modify Project Name

--- src/main/java/com/spring/utils/archive/SpringUtilsArchiveApplication.java
+++ src/main/java/com/spring/utils/archive/SpringUtilsArchiveApplication.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
-public class SpringArchiveApplication {
+public class SpringUtilsArchiveApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringArchiveApplication.class, args);
+        SpringApplication.run(SpringUtilsArchiveApplication.class, args);
     }
 
 }
